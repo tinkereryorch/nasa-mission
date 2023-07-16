d4761e45bd70046fad5f904e8810fdccf4b3bcdd
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-16 13:42:14 -0500

    Implemented httpGetPlanets

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,6 +1,8 @@
+const BASE_API_URL = 'http://localhost:8000';
+
 async function httpGetPlanets() {
-  // TODO: Once API is ready.
-  // Load planets and return as JSON.
+  const response = await fetch(`${BASE_API_URL}/planets`);
+  return await response.json();
 }
 
 async function httpGetLaunches() {
