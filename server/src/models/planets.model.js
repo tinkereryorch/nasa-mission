5b3d320c982fa7490f9e568c0cf3286e07f88001
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-20 23:08:12 -0500

    Added loadPlanetsData function

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -9,23 +9,27 @@ function isPlanetHabitable(planet) {
         && planet['koi_prad'] < 1.6;
 }
 
-fs.createReadStream('../../data/kepler_data.csv')
-    .pipe(parse({
-        comment: '#',
-        columns: true
-    }))
-    .on('data', (data) => {
-        if (isPlanetHabitable(data)) {
-            habitablePlanets.push(data);
-        }
-    })
-    .on('error', (error) => {
-        console.log(error);
-    })
-    .on('end', () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`);
-        console.log('Done');
-    });
+function loadPlanetsData() {
+
+    fs.createReadStream('../../data/kepler_data.csv')
+        .pipe(parse({
+            comment: '#',
+            columns: true
+        }))
+        .on('data', (data) => {
+            if (isPlanetHabitable(data)) {
+                habitablePlanets.push(data);
+            }
+        })
+        .on('error', (error) => {
+            console.log(error);
+        })
+        .on('end', () => {
+            console.log(`${habitablePlanets.length} habitable planets found!`);
+            console.log('Done');
+        });
+
+}
 
 module.exports = {
     planets: habitablePlanets,
