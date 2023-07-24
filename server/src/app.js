8ef40cfe172dfb602fecec0848c64e0f1adf8e71
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-23 23:20:03 -0500

    Added frontend entry route in express

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const planetsRouter = require('./routes/planets.router');
 
@@ -9,6 +10,9 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 module.exports = app;
