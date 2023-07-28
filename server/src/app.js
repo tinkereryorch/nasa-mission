e4675fe55984d209f5d7f2c91ee0b563083e3701
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-27 22:51:16 -0500

    Updated planets router path

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
 
-const planetsRouter = require('./routes/planets.router');
+const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
