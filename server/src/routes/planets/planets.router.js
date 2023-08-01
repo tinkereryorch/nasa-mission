227b565e802b9dd9506934941d48fbbc125e12a3
Jorge Garcia <jegcelorio@yahoo.com>
2023-07-31 22:38:47 -0500

    refactored planets router

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -3,6 +3,6 @@ const planetsController = require('./planets.controller');
 
 const planetsRouter = express.Router();
 
-planetsRouter.get('/planets', planetsController.getAllPlanets);
+planetsRouter.get('/planets', planetsController.httpGetAllPlanets);
 
 module.exports = planetsRouter;
