eac1ec331086f3ebc3b9c06dd9e86f10aceab6cf
Jorge Garcia <jegcelorio@yahoo.com>
2023-08-03 23:14:22 -0500

    Updated app routes

--- server/src/app.js
+++ server/src/app.js
@@ -17,9 +17,9 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use(planetsRouter);
-app.use(launchesRouter);
-app.get('/', (req, res) => {
+app.use('/planets', planetsRouter);
+app.use('/launches', launchesRouter);
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 module.exports = app;
