a342befa6279c8db5ab90f5dda607554c842a557
Jorge Garcia <jegcelorio@yahoo.com>
2023-08-06 13:38:11 -0500

    Implemented httpSubmitLaunch

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -5,18 +5,28 @@ async function httpGetPlanets() {
   return await response.json();
 }
 
-// Load launches, sort by flight number, and return as JSOn
+// Load launches, sort by flight number, and return as JSON
 async function httpGetLaunches() {
   const response = await fetch(`${BASE_API_URL}/launches`);
-  const fetchedLaunches = response.json();
+  const fetchedLaunches = await response.json();
   return fetchedLaunches.sort((a, b) => { 
     return a.flightNumber - b.flightNumber; 
   });
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try{
+    return await fetch(`${BASE_API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type" : "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch(err) {
+    return { ok: false, };
+  }
 }
 
 async function httpAbortLaunch(id) {
