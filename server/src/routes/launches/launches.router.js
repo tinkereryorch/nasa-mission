899fb6e08262c5da834e5232e15f9e2a575c73e0
Jorge Garcia <jegcelorio@yahoo.com>
2023-08-06 21:17:12 -0500

    Added route to delete launch

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -5,5 +5,6 @@ const launchesRouter = express.Router();
 
 launchesRouter.get('/', launchesController.httpGetAllLaunches);
 launchesRouter.post('/', launchesController.httpAddNewLaunch);
+launchesRouter.delete('/:id', launchesController.httpAbortLaunch)
 
 module.exports = launchesRouter;
