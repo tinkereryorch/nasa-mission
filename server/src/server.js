3fa745aa18c126d5c0b5d1e4c25f2010dd4b029a
Jorge Garcia <jegcelorio@yahoo.com>
2023-08-22 21:21:31 -0500

    Added mongoose connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,25 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const app = require('./app');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://username:password@nasacluster.p1jmzly.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (error) => {
+    console.error(error);
+});
+
 async function StartServer() {
+    await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
 
     server.listen(PORT, () => {
