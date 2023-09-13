9fe05fe083ab48533cbfedc3ad4df964a8e4dcfd
Jorge Garcia <jegcelorio@yahoo.com>
2023-09-12 20:17:00 -0500

    Updated unit tests with calls to v1

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -5,8 +5,6 @@ const {
     mongoDisconnect 
 } = require('../../services/mongo');
 
-
-
 describe('Launches API', () => {
 
     beforeAll(async () => { 
@@ -40,7 +38,7 @@ describe('Launches API', () => {
     describe('Test GET /launches', () => {
         test('It should respond with 200 success', async () => {
             const response = await request(app)
-                .get('/launches')
+                .get('/v1/launches')
                 .expect('Content-Type', /json/)
                 .expect(200);
         });
@@ -50,7 +48,7 @@ describe('Launches API', () => {
         test('It should respond with 201 success', async () => {
 
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(completeLaunchData)
                 .expect('Content-Type', /json/)
                 .expect(201);
@@ -63,7 +61,7 @@ describe('Launches API', () => {
 
         test('It should catch missing required properts', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(launchDataWithoutDate)
                 .expect('Content-Type', /json/)
                 .expect(400);
@@ -74,7 +72,7 @@ describe('Launches API', () => {
         });
         test('It should catch invalid dates', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(launchDataWithInvalidDate)
                 .expect('Content-Type', /json/)
                 .expect(400);
